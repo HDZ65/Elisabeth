472bed4002e6474585823215ac2dad018d1d64d1
Alexandre <alexandre.hernandez@yahoo.com>
2024-04-30 12:51:51 +0200

    30/04

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import React  from 'react';
+import React, { Suspense } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 
 const Accueil = React.lazy(() => import("./pages/Accueil"));
-
+const Coaching = React.lazy(() => import("./pages/Coaching"));
+const Ebook = React.lazy(() => import("./pages/Ebook"));
+const Blog = React.lazy(() => import("./pages/Blog"));
+const Contact = React.lazy(() => import("./pages/Contact"));
 
 function App() {
   return (
     <Router>
+      <Suspense fallback={<div>Chargement...</div>}>
         <Routes>
           <Route path="/" element={<Accueil />} />
-
+          <Route path="/coaching" element={<Coaching />} />
+          <Route path="/e-book" element={<Ebook />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
+      </Suspense>
     </Router>
   );
 }
