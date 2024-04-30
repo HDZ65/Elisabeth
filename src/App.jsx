3385166eb7a3b55b13dfb6db8d2a8fe499750a8e
Alexandre <alexandre.hernandez@yahoo.com>
2024-04-30 12:46:45 +0200

    sxc

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,14 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 
 const Accueil = React.lazy(() => import("./pages/Accueil"));
-const Coaching = React.lazy(() => import("./pages/Coaching"));
-const Ebook = React.lazy(() => import("./pages/Ebook"));
-const Blog = React.lazy(() => import("./pages/Blog"));
-const Contact = React.lazy(() => import("./pages/Contact"));
+
 
 function App() {
   return (
     <Router>
         <Routes>
           <Route path="/" element={<Accueil />} />
-          <Route path="/coaching" element={<Coaching />} />
-          <Route path="/e-book" element={<Ebook />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/contact" element={<Contact />} />
+
         </Routes>
     </Router>
   );
